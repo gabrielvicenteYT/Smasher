b508393aa264d8e47b1a4581d8d7b840de167887
Frank Mayer <frank.mayer19072001@outlook.de>
2021-03-24 17:21:50 +0100

    Initial commit

---
+++ src/main/kotlin/de/kuerbisskraft/smasher/Smasher.kt
@@ -0,0 +1,13 @@
+package de.kuerbisskraft.smasher
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class Smasher : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
